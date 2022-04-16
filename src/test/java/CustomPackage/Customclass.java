2886e4554359233bce97e17f3f259bfe196d39c5
Dhiraj <Dhirajquality@gmail.com>
2022-04-17 03:27:43 +0530

    Guru 99 Project

--- src/test/java/CustomPackage/Customclass.java
+++ src/test/java/CustomPackage/Customclass.java
@@ -6,29 +6,40 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Customclass
 {
-//	WebDriver dr ;
-	public void Set_Text(String Bys,String Locator,String fieldText,WebDriver dr) 
+WebDriver dr ;
+public Customclass(WebDriver dr)
+{
+	this.dr = dr;
+	
+}
+	public void Set_Text(String Bys,By Locator,String fieldText) 
 	{
 		switch (Bys) 
 		{
 		case "XPATH":
-				dr.findElement(By.xpath(Locator)).sendKeys(fieldText);
+				dr.findElement(Locator).sendKeys(fieldText);
 			break;
 		case "ID":
-				dr.findElement(By.id(Locator)).sendKeys(fieldText);
+				dr.findElement(Locator).sendKeys(fieldText);
 			break;
+		case "NAME":
+			dr.findElement(Locator).sendKeys(fieldText);
+		break;
 		}
 	}
-	public void Click_Button(String Bys,String Locator,WebDriver dr) 
+	public void Click_Button(String Bys,By Locator) 
 	{
 		switch (Bys) 
 		{
 		case "XPATH":
-				dr.findElement(By.xpath(Locator)).click();;
+				dr.findElement(Locator).click();;
 			break;
 		case "ID":
-				dr.findElement(By.id(Locator)).click();;
+				dr.findElement(Locator).click();;
 			break;
+		case "NAME":
+			dr.findElement(Locator).click();;
+		break;
 		}
 	}
 }
