727e0be57171b3acc59497be1dbc5b21b0d5fcea
Dhiraj <Dhirajquality@gmail.com>
2022-04-17 03:53:25 +0530

    Commot

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -8,25 +8,59 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import Pages.*;
+import GooglePage.*;
 
 public class NewTest
 {
 	WebDriver dr;
-	
-
   @Test()
   public void f1()
   {
 	  System.setProperty("webdriver.gecko.driver", "D:\\Software\\geckodriver.exe");
 	  dr = new FirefoxDriver();
 	  dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
-	  dr.get("https://www.demo.guru99.com/V4/");
-	  new Login_Page(dr).Login("mngr399806" ,"gyjYqat");
-	  
+	  dr.get("https://www.google.com/");
+	  String SearchWord = "Hello World";
+	  GoogleHomePage GHP = new GoogleHomePage(dr);
+	  GHP.Homepage(SearchWord);
+	  GHP.Verify(SearchWord);
   }
-  
+  @Test()
+  public void f2()
+  {
+	  System.setProperty("webdriver.gecko.driver", "D:\\Software\\geckodriver.exe");
+	  dr = new FirefoxDriver();
+	  dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+	  dr.get("https://www.google.com/");
+	  String SearchWord = "Hello Java";
+	  GoogleHomePage GHP = new GoogleHomePage(dr);
+	  GHP.Homepage(SearchWord);
+	  GHP.Verify(SearchWord);
+  }
+  @Test()
+  public void f3()
+  {
+	  System.setProperty("webdriver.gecko.driver", "D:\\Software\\geckodriver.exe");
+	  dr = new FirefoxDriver();
+	  dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+	  dr.get("https://www.google.com/");
+	  String SearchWord = "Hello Java";
+	  GoogleHomePage GHP = new GoogleHomePage(dr);
+	  GHP.Homepage(SearchWord);
+	  GHP.Verify(SearchWord);
+  }
+  @Test()
+  public void f4()
+  {
+	  System.setProperty("webdriver.gecko.driver", "D:\\Software\\geckodriver.exe");
+	  dr = new FirefoxDriver();
+	  dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+	  dr.get("https://www.google.com/");
+	  String SearchWord = "Hello Java";
+	  GoogleHomePage GHP = new GoogleHomePage(dr);
+	  GHP.Homepage(SearchWord);
+	  GHP.Verify(SearchWord);
+  }
   @AfterMethod
   public void ExitFirefox()
   {
